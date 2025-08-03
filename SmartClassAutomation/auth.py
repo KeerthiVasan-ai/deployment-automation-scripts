bf8ffbeda84bc84985cc64c30c8d49559e8c7d6c
KeerthiVasan-ai <keerthi77459@gmail.com>
2025-08-03 23:32:31 +0530

    UAT

---
+++ SmartClassAutomation/auth.py
@@ -0,0 +1,8 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+def auth():
+    cred = credentials.Certificate("firebase_key.json")
+    firebase_admin.initialize_app(cred)
+    print("Success")
+    return firestore.client()
