7e78aa64812f3264cf1f0655a5e4062c7abe8300
KeerthiVasan-ai <keerthi77459@gmail.com>
2025-08-03 23:33:31 +0530

    UAT

--- SmartClassAutomation/insert_slot.prod.py
+++ SmartClassAutomation/insert_slot.prod.py
@@ -13,15 +13,15 @@ def insert_time_slots(days_from_today: int = 21):
     time_slots = generate_time_slots()
 
     # # Reference to the Firestore document
-    slot_doc_ref = (
-        db.collection('timeSlots')
-          .document(target_date_str)
-          .collection('availability')
-          .document('slots')
-    )
+    # slot_doc_ref = (
+    #     db.collection('timeSlots')
+    #       .document(target_date_str)
+    #       .collection('availability')
+    #       .document('slots')
+    # )
 
-    # Set time slots in Firestore
-    slot_doc_ref.set(time_slots)
+    # # Set time slots in Firestore
+    # slot_doc_ref.set(time_slots)
     print(f"✅ Time slots inserted for {target_date_str}")
     return 'Time slots updated successfully'
 
