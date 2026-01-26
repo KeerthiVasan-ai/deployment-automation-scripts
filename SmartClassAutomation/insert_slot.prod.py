529e021992b2176f616f300f349d80bca390a1e5
Keerthivasan <Keerthivasan@thirantech.com>
2026-01-26 12:57:04 +0530

    chore: improvement for even sem

--- SmartClassAutomation/insert_slot.prod.py
+++ SmartClassAutomation/insert_slot.prod.py
@@ -9,8 +9,10 @@ def insert_time_slots(days_from_today: int = 21):
     db = auth.auth()
     target_date = datetime.date.today() + datetime.timedelta(days=days_from_today)
     target_date_str = target_date.strftime('%Y-%m-%d')
-
-    time_slots = generate_time_slots()
+    
+    isTarget = is_target_day(target_date)
+    
+    time_slots = generate_time_slots(isTarget)
 
     # Reference to the Firestore document
     slot_doc_ref = (
@@ -25,20 +27,33 @@ def insert_time_slots(days_from_today: int = 21):
     print(f"✅ Time slots inserted for {target_date_str}")
     return 'Time slots updated successfully'
 
+def is_target_day(date):
+    return date.weekday() in (0, 1, 2, 3)
 
-def generate_time_slots():
+def generate_time_slots(isTarget):
     """
     Returns the default time slots dictionary.
     """
-    return {
-        '08:30AM': True,
-        '09:30AM': True,
-        '10:40AM': True,
-        '11:40AM': True,
-        '01:30PM': True,
-        '02:30PM': True,
-        '03:30PM': True,
-    }
+    if(isTarget):
+        return {
+            '08:30AM': True,
+            '09:30AM': True,
+            '10:40AM': True,
+            '11:40AM': True,
+            '01:30PM': False,
+            '02:30PM': False,
+            '03:30PM': False,
+        }
+    else: 
+        return {
+            '08:30AM': True,
+            '09:30AM': True,
+            '10:40AM': True,
+            '11:40AM': True,
+            '01:30PM': True,
+            '02:30PM': True,
+            '03:30PM': True,
+        }
 
 
 if __name__ == "__main__":
